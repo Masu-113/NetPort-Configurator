b0b1d143f9d0a96f2badc0477a517e479a2a7709
msuarez <msuarez@IDEAY-WKS107>
2025-08-12 09:12:18 -0600

    corrección de main.rs y lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,13 +25,48 @@ async fn ejecutar_powershell() -> Result<String, String> {
         Err(s.trim().to_string())
     }  
 }
+ 
+#[tauri::command]
+async fn cambiar_vlan(interface_name: String, new_vlan_id: String) -> Result<String, String> {
+    let script = format!(
+        r#"
+        $interfaceName = "{}"
+        $newVlanId = {}
+        
+        try {{
+            Set-NetAdapterAdvancedProperty -Name $interfaceName -DisplayName "VLAN ID" -DisplayValue $newVlanId
+            "VLAN ID cambiado a: $newVlanId"
+        }} catch {{
+            "No se pudo cambiar el VLAN ID: $_"
+        }}
+        "#,
+        interface_name, new_vlan_id
+    );
+
+    let output = tauri::async_runtime::spawn_blocking(move || {
+        std::process::Command::new("powershell.exe")
+            .args(&["-ExecutionPolicy", "Bypass", "-Command", &script])
+            .output()
+    })
+    .await
+    .map_err(|e| e.to_string())?
+    .map_err(|e| e.to_string())?;
+
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout).to_string();
+        Ok(s.trim().to_string())
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr).to_string();
+        Err(s.trim().to_string())
+    }
+}
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![ejecutar_powershell])
+        .invoke_handler(tauri::generate_handler![ejecutar_powershell, cambiar_vlan])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
