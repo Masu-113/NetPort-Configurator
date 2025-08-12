145c44d27591ee599dea36bb81ed3bd24edb2704
msuarez <msuarez@IDEAY-WKS107>
2025-08-12 07:30:12 -0600

    cambios en los archivos lib.rs y main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,39 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!! 
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Builder;
-
-#[tauri::command]
-async fn ejecutar_powershell() -> Result<String, String> {
-    let script_path = r"C:\Users\msuarez\tauri-app\src-ps1\get_network_info.ps1".to_string();
-
-    let output = tauri::async_runtime::spawn_blocking(move || {
-        std::process::Command::new("powershell.exe")
-            .args(&["-ExecutionPolicy", "Bypass", "-File", &script_path])
-            .output()
-    })
-    .await
-    .map_err(|e| e.to_string())?
-    .map_err(|e| e.to_string())?;
-
-    println!("RAW stdout: {:?}", output.stdout);
-    println!("RAW stderr: {:?}", output.stderr);
-
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout).to_string();
-        println!("stdout text: {:?}", s);
-        Ok(s.trim().to_string())
-    } else {
-        let s = String::from_utf8_lossy(&output.stderr).to_string();
-        println!("stderr text: {:?}", s);
-        Err(s.trim().to_string())
-    }
-    
-}
-
 fn main() {
-    Builder::default()
-        .invoke_handler(tauri::generate_handler![ejecutar_powershell]) 
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    tauri_app_lib::run()
 }
