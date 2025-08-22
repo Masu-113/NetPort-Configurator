2ebd9a3f1c7a9e6f0c66e67789b22e69885fa82e
msuarez <msuarez@IDEAY-WKS107>
2025-08-22 14:55:24 -0600

    se modificaron las rutas de is .ps1 a rutas absolutas

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,14 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::path::PathBuf;
+use std::process::Command;
+
 #[tauri::command]
 async fn ejecutar_powershell() -> Result<String, String> {
-    let script_path = "../src-ps1/get_network_info.ps1".to_string();
+    let script_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src-ps1/get_network_info.ps1");
 
     let output = tauri::async_runtime::spawn_blocking(move || {
         std::process::Command::new("powershell.exe")
-            .args(&["-ExecutionPolicy", "Bypass", "-File", &script_path])
+            .args(&["-ExecutionPolicy", "Bypass", "-File", script_path.to_str().unwrap()])
             .output()
     })
     .await
@@ -23,46 +26,8 @@ async fn ejecutar_powershell() -> Result<String, String> {
         let s = String::from_utf8_lossy(&output.stderr).to_string();
         println!("stderr text: {:?}", s);
         Err(s.trim().to_string())
-
     }  
 }
- 
-#[tauri::command]
-async fn cambiar_vlan(interface_name: String, new_vlan_id: String) -> Result<String, String> {
-    let script = format!(
-        r#"
-        $interfaceName = "{}"
-        $newVlanId = {}
-        
-        try {{
-            Set-NetAdapterAdvancedProperty -Name $interfaceName -DisplayName "VLAN ID" -DisplayValue $newVlanId
-            "VLAN ID cambiado a: $newVlanId"
-        }} catch {{
-            "No se pudo cambiar el VLAN ID: $_"
-        }}
-        "#,
-        interface_name, new_vlan_id
-    );
-
-    let output = tauri::async_runtime::spawn_blocking(move || {
-        std::process::Command::new("powershell.exe")
-            .args(&["-ExecutionPolicy", "Bypass", "-Command", &script])
-            .output()
-    })
-    .await
-    .map_err(|e| e.to_string())?
-    .map_err(|e| e.to_string())?;
-
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout).to_string();
-        Ok(s.trim().to_string())
-    } else {
-        let s = String::from_utf8_lossy(&output.stderr).to_string();
-        Err(s.trim().to_string())
-    }
-}
-
-use std::process::Command;
 
 #[tauri::command]
 fn cambiar_config_puerto(datos: serde_json::Value) -> Result<(), String> {
@@ -72,14 +37,15 @@ fn cambiar_config_puerto(datos: serde_json::Value) -> Result<(), String> {
     let vlan = datos["vlan"].as_str().unwrap_or("");
 
     println!("Datos enviados: nombre={}, ip={}, mask={}, vlan={}", nombre, ip, mask, vlan);
+    let script_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src-ps1/change_conf_port.ps1");
 
     let output = Command::new("powershell.exe")
         .arg("-ExecutionPolicy")
         .arg("Bypass")
         .arg("-NoProfile")
         .arg("-Command")
-        .arg(format!(
-            "& '../src-ps1/change_conf_port.ps1' -nombre '{}' -ip '{}' -mask '{}' -vlan '{}'",
+        .arg(format!("& '{}' -nombre '{}' -ip '{}' -mask '{}' -vlan '{}'",
+            script_path.to_str().unwrap(),
             nombre, ip, mask, vlan
         ))
         .output()
@@ -100,7 +66,7 @@ fn cambiar_config_puerto(datos: serde_json::Value) -> Result<(), String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![ejecutar_powershell, cambiar_vlan, cambiar_config_puerto])
+        .invoke_handler(tauri::generate_handler![ejecutar_powershell, cambiar_config_puerto])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
