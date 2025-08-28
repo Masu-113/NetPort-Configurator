d373a0a8c8878096bcc094abe0a5074f75919409
msuarez <msuarez@IDEAY-WKS107>
2025-08-28 10:47:09 -0600

    se modifico el build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,33 +1,3 @@
-use tauri_build::WindowsAttributes;
-
 fn main() {
-    let mut windows = WindowsAttributes::new();
-
-    windows = windows.app_manifest(
-        r#"
-        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-            <dependency>
-                <dependentAssembly>
-                    <assemblyIdentity 
-                        type="win32" 
-                        name="Microsoft.Windows.Common-Controls" 
-                        version="5.82.0.0" 
-                        processorArchitecture="*" 
-                        publicKeyToken="6595b64144ccf1df" 
-                        language="*"/>
-                </dependentAssembly>
-            </dependency>
-            <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-                <security>
-                    <requestedPrivileges>
-                        <requestedExecutionLevel level="requireAdministrator" uiAccess="false"/>
-                    </requestedPrivileges>
-                </security>
-            </trustInfo>
-        </assembly>
-        "#
-    );
-
-    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
-    tauri_build::try_build(attrs).expect("failed to run build script");
+    tauri_build::build()
 }
