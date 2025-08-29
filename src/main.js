3bfff2b15029cfb7ff2a74b80c3fc1ae20d824fb
msuarez <msuarez@IDEAY-WKS107>
2025-08-29 15:41:02 -0600

    correccion en la validacion del ip

--- src/main.js
+++ src/main.js
@@ -87,7 +87,7 @@ async function guardarCambios() {
 
 // -------- Funcion para validar los datos ingresados a modificar ------- //
 function validar_datos(datos) {
-  const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+   const ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   const soloNumeros = /^\d+$/;
 
   if (!datos.nombre || !datos.ip || !datos.mask) {
